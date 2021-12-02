a05f4554ca13d2a510f4ef9b632c967c2e3103b2
User <hoshimovtolibjon1007@mail.com>
2021-12-02 17:09:01 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+
+new Vue({
+    el:"#app",
+    data:{
+        checked:[]
+    }
+})
+
+
+new Vue({
+    el:".container",
+    data:{
+        check1:false,
+        check2:false,
+        check3:false
+    }
+})
