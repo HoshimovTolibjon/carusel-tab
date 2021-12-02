112ae9bb8ca782b8da958c5db69dc42678defaca
User <hoshimovtolibjon1007@mail.com>
2021-12-02 17:09:01 +0500

    kflgjljg

---
+++ carusel.js
@@ -0,0 +1,21 @@
+
+new Vue({
+    el:".container",
+    data:{
+
+        carusel:[
+            {
+                image:"https://wallpaperaccess.com/full/31189.jpg",
+                name:"Tabiat"
+            },
+            {
+                image:"https://i.imgur.com/hunyCzO.jpg",
+                name:"Ko'l"
+            },
+            {
+                image:"https://wallpaperaccess.com/full/969650.jpg",
+                name:"Sharshara"
+            }
+        ]
+    }
+})
